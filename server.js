d53dfb004a786ca79ea24bcb889b83619acbab1a
Anssi Siren <pro@avaruus.mozmail.com>
2025-05-06 20:20:10 +0300

    Add support for command line arguments

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import { createWriteStream } from 'node:fs'
 import concurrently from 'concurrently'
+import yargs from 'yargs'
+
+const argv = yargs(process.argv.slice(2)).parse()
+const volume = typeof argv.volume === 'number' ? argv.volume : 0
+const tolerant = argv.tolerant || false
 
 const geometries = [
   '50%x50%+0+0',
@@ -10,16 +15,33 @@ const geometries = [
 
 const genCmd = (() => {
   let counter = -1
-  return (ipAddr, { channel = 0, port = 554, user = 'user', pass = 'pass' } = {}) => (
-    `mpv '--geometry=${geometries[counter += 1] || '50%x50%'}' 'rtsp://${user}:${pass}@${ipAddr}:${port}/live/ch${channel}'`)
+  return (ipAddr, name = 'anonymous', { channel = 0, port = 554, user = 'user', pass = 'pass' } = {}) => ({
+    command: [
+      'mpv',
+      `--border=no`,
+      `--volume=${volume}`,
+      `--geometry=${geometries[counter += 1] || '50%x50%'}`,
+      `rtsp://${user}:${pass}@${ipAddr}:${port}/live/ch${channel}`,
+    ].join(' '),
+    name,
+  })
 })()
 
-concurrently([
-  genCmd('192.168.10.82'),
-  genCmd('192.168.10.89'),
+const conc = await concurrently([
+  genCmd('192.168.10.82', 'kelder'),
+  genCmd('192.168.10.89', 'park'),
 ], {
-  killOthers: ['success', 'failure'],
+  killOthers: tolerant ? [] : ['success', 'failure'],
   prefixColors: 'auto',
-  outputStream: createWriteStream('/dev/null'),
+  raw: true,
+  // outputStream: createWriteStream('/dev/null'), // Not needed with { raw: true }
 })
+
+try {
+  await conc.result
+} catch (errors) {
+  for (const { command: { command, name }, index, killed, exitCode, timings: { durationSeconds } } of errors) {
+    console.table({ command, name, index, killed, exitCode, durationSeconds })
+  }
+}
 
